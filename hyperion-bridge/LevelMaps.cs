40e066eb8ef408fc964a091a3417b4d2c525fb53
Justin Unterreiner <justin.unterreiner@gmail.com>
2024-01-29 23:07:06 -0800

    Refactor for level 1 mapping

---
+++ hyperion-bridge/LevelMaps.cs
@@ -0,0 +1,28 @@
+
+public class LevelMaps
+{
+    public static Dictionary<int, Dictionary<int, List<int>>> Mapping = new Dictionary<int, Dictionary<int, List<int>>>()
+    {
+        // Level 1
+        { 1, new Dictionary<int, List<int>>()
+            {
+                { 0, new List<int>() { 42, 43, 44, 45 } },
+                { 1, new List<int>() { 38, 39, 40, 41 } },
+                { 2, new List<int>() { 36, 37 } },
+                { 3, new List<int>() { 34, 35 } },
+                { 4, new List<int>() { 31, 32, 33 } },
+                { 5, new List<int>() { 29, 30 } },
+                { 6, new List<int>() { 25, 26, 27, 28 } },
+                { 7, new List<int>() { 21, 22, 23, 24 } },
+                { 8, new List<int>() { 17, 18, 19, 20 } },
+                { 9, new List<int>() { 13, 14, 15, 16 } },
+                { 10, new List<int>() { 8, 9, 10, 11, 12 } },
+                { 11, new List<int>() { 5, 6, 7 } },
+                { 12, new List<int>() { 1, 2, 3, 4 } },
+                { 13, new List<int>() { 0, 54, 55 } },
+                { 14, new List<int>() { 50, 51, 52, 53 } },
+                { 15, new List<int>() { 46, 47, 48, 49 } },
+            }
+        }
+    };
+}
--- hyperion-bridge/Program.cs
+++ hyperion-bridge/Program.cs
@@ -5,25 +5,116 @@ using System.Text;
 
 class Program
 {
-    public static void Main()
+    const string HYPERION_IP_ADDRESS = "127.0.0.1";
+    const int HYPERION_JSON_PORT = 19444;
+    const string HYPERION_ORIGIN = "tempest-hyperion-bridge-cli";
+    const int LED_COUNT = 64;
+    const int MAX_LED_INDEX = 56;
+
+    public static async Task Main()
     {
-        DoWork().Wait();
+        Console.WriteLine($"[hyperion-bridge] Connecting to Hyperion JSON socket '{HYPERION_IP_ADDRESS}:{HYPERION_JSON_PORT}'...");
+        using var socket = await Connect(HYPERION_IP_ADDRESS, HYPERION_JSON_PORT);
+
+        if (!Console.IsInputRedirected)
+        {
+            Console.WriteLine("[hyperion-bridge] Standard input not detected; running chaser demo...");
+            await RunChaserDemo(socket);
+            return;
+        }
+
+        Console.WriteLine("[hyperion-bridge] Standard input not detected; reading input...");
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+
+            // End of standard input; exit program.
+            if (input == null)
+                return;
+
+            // TODO: Handle other commands.
+            var isPlayerPosition = input.StartsWith("Player Position: ");
+
+            if (!isPlayerPosition)
+                continue;
+
+            var positionString = input.Split("Player Position: ")[1];
+            var parsedOk = int.TryParse(positionString, out int position);
+
+            if (!parsedOk)
+                continue;
+
+            // TODO: Use real level.
+            await RenderGameplay(socket, 0, position);
+        }
     }
 
-    // G R B
-
-    public static async Task DoWork()
+    private static async Task<Socket> Connect(string ipAddress, int port)
     {
-        Console.WriteLine("Starting...");
-
-        IPEndPoint ipEndPoint = new(IPAddress.Parse("127.0.0.1"), 19444);
-
-        using Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        var ipEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+        var client = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
         await client.ConnectAsync(ipEndPoint);
+        return client;
+    }
 
-        var json1 = "{\"command\": \"color\", \"color\": [0,0,255], \"priority\": 50, \"origin\": \"test command line\"}\r\n";
+    private static async Task RenderGameplay(Socket socket, int level, int position)
+    {
+        var ledIndicies = GetLedIndiciesForPlayer(level, position);
+
+        var sb = new StringBuilder();
+
+        var colorJson = sb.ToString();
+
+        for (var i = 0; i < LED_COUNT; i++)
+        {
+            if (i != 0)
+            {
+                sb.Append(",");
+            }
+
+            if (i >= MAX_LED_INDEX)
+            {
+                // Unused LEDs.
+                sb.Append("0,0,0");
+            }
+            else if (ledIndicies.Contains(i))
+            {
+                // Player occupied level segment
+                // TODO: Does ship change color like the level does?
+                sb.Append("192,192,0"); // Yellow
+            }
+            else
+            {
+                // Unoccupied level segment
+                // TODO: Set correct background color based on level.
+                sb.Append("192,192,0"); // Blue
+            }
+        }
+
+        var json = $$"""
+            {"command": "color", "color": [{{colorJson}}], "priority": 50, "origin": "{{HYPERION_ORIGIN}}"}\r\n
+            """;
+
+        var messageBytes = Encoding.ASCII.GetBytes(json);
+        await socket.SendAsync(messageBytes, SocketFlags.None);
+    }
+
+    private static List<int> GetLedIndiciesForPlayer(int level, int position)
+    {
+        var levelMap = LevelMaps.Mapping[level];
+        var positionMap = levelMap[position];
+        return positionMap;
+    }
+
+    public static async Task RunChaserDemo(Socket socket)
+    {
+        // Clear to blue; colors use G,R,B format.
+        var json1 = $$"""
+            {"command": "color", "color": [0,0,255], "priority": 50, "origin": "{{HYPERION_ORIGIN}}"}\r\n"
+            """;
         var messageBytes1 = Encoding.ASCII.GetBytes(json1);
-        var result1 = await client.SendAsync(messageBytes1, SocketFlags.None);
+        await socket.SendAsync(messageBytes1, SocketFlags.None);
 
         var location = 0;
         var incrementBy = 1;
@@ -42,33 +133,34 @@ class Program
 
                 if (i >= 56)
                 {
+                    // Unused LEDs.
                     sb.Append("0,0,0");
                 }
                 else if (location >= i - range && location <= i + range)
                 {
+                    // Yellow chaser dots
                     sb.Append("192,192,0");
                 }
                 else
                 {
-                    // sb.Append("0,0,255");
+                    // Blue background
                     sb.Append("0,0,128");
                 }
             }
 
             var colorJson = sb.ToString();
-            var json2 = "{\"command\": \"color\", \"color\": [" + colorJson + "], \"priority\": 50, \"origin\": \"test command line\"}\r\n";
+            var json2 = $$"""
+                {"command": "color", "color": [{{colorJson}}], "priority": 50, "origin": "{{HYPERION_ORIGIN}}"}\r\n
+                """;
             var messageBytes2 = Encoding.ASCII.GetBytes(json2);
-            var result2 = await client.SendAsync(messageBytes2, SocketFlags.None);
+            await socket.SendAsync(messageBytes2, SocketFlags.None);
 
             location += incrementBy;
 
             if (location >= 56 || location <= 0)
-            {
                 incrementBy = incrementBy * -1;
-            }
 
-            System.Threading.Thread.Sleep(25);
-            // System.Threading.Thread.Sleep(5);
+            Thread.Sleep(25);
         }
     }
 }
