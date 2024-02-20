cc4bb31525927eb541847097fb9c64e6bbdcefda
Justin Unterreiner <justin.unterreiner@gmail.com>
2024-02-19 23:07:27 -0800

    Added a few more level mappings

--- hyperion-bridge/LevelMaps.cs
+++ hyperion-bridge/LevelMaps.cs
@@ -23,6 +23,116 @@ public class LevelMaps
                 { 14, new List<int>() { 50, 51, 52, 53 } },
                 { 15, new List<int>() { 46, 47, 48, 49 } },
             }
-        }
+        },
+
+        // Level 2
+        { 2, new Dictionary<int, List<int>>()
+            {
+                { 0, new List<int>() { 42, 43, 44, 45} },
+                { 1, new List<int>() { 39, 40, 41 } },
+                { 2, new List<int>() { 36, 37, 38 } },
+                { 3, new List<int>() { 33, 34, 35 } },
+                { 4, new List<int>() { 31, 32, 33 } },
+                { 5, new List<int>() { 29, 29, 30 } },
+                { 6, new List<int>() { 25, 26, 27, 28 } },
+                { 7, new List<int>() { 20, 31, 22, 23, 24 } },
+                { 8, new List<int>() { 16, 17, 19, 19 } },
+                { 9, new List<int>() { 11, 12, 13, 14, 15 } },
+                { 10, new List<int>() { 8, 9, 10 } },
+                { 11, new List<int>() { 4, 5, 6, 7 } },
+                { 12, new List<int>() { 1, 2, 3, 4 } },
+                { 13, new List<int>() { 55, 0, 1 } },
+                { 14, new List<int>() { 51, 52, 53, 54, 55 } },
+                { 15, new List<int>() { 46, 47, 49, 49, 50 } },
+            }
+        },
+
+        // Level 3
+        { 3, new Dictionary<int, List<int>>()
+            {
+                { 0, new List<int>() { 41, 42, 43, 44, 45 } },
+                { 1, new List<int>() { 38, 39, 40, 41 } },
+                { 2, new List<int>() { 36, 37 } },
+                { 3, new List<int>() { 33, 34, 35 } },
+                { 4, new List<int>() { 31, 32, 33 } },
+                { 5, new List<int>() { 29, 30 } },
+                { 6, new List<int>() { 25, 26, 27, 28 } },
+                { 7, new List<int>() { 20, 31, 22, 23, 24, 25 } },
+                { 8, new List<int>() { 16, 17, 18, 19, 20 } },
+                { 9, new List<int>() { 15 } },
+                { 10, new List<int>() { 8, 9, 10, 11, 12, 13, 14 } },
+                { 11, new List<int>() { 4, 5, 6, 7, 8 } },
+                { 12, new List<int>() { 0, 1, 2, 3, 4 } },
+                { 13, new List<int>() { 0, 52, 53, 54, 55 } },
+                { 14, new List<int>() { 51 } },
+                { 15, new List<int>() { 46, 47, 48, 49, 50 } },
+            }
+        },
+
+        // Level 4
+        { 4, new Dictionary<int, List<int>>()
+            {
+                { 0, new List<int>() { 41, 42, 43, 44, 45 } },
+                { 1, new List<int>() { 37, 38, 39, 40, 41 } },
+                { 2, new List<int>() { 35, 36, 37 } },
+                { 3, new List<int>() { 32, 33, 34 } },
+                { 4, new List<int>() { 29, 30, 31, 32 } },
+                { 5, new List<int>() { 25, 26, 27, 28, 29 } },
+                { 6, new List<int>() { 21, 22, 23, 24 } },
+                { 7, new List<int>() { 18, 19, 20 } },
+                { 8, new List<int>() { 15, 16, 17 } },
+                { 9, new List<int>() { 11, 12, 13, 14 } },
+                { 10, new List<int>() { 9, 10, 11 } },
+                { 11, new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8 } },
+                { 12, new List<int>() { 0, 55, 54 } },
+                { 13, new List<int>() { 52, 53, 54 } },
+                { 14, new List<int>() { 49, 50, 51 } },
+                { 15, new List<int>() { 45, 46, 47, 48 } },
+            }
+        },
+
+        // Level 5
+        { 5, new Dictionary<int, List<int>>()
+            {
+                { 0, new List<int>() { 39, 40, 41, 42, 43 } },
+                { 1, new List<int>() { 35, 36, 37, 38 } },
+                { 2, new List<int>() { 34, 35 } },
+                { 3, new List<int>() { 32, 33, 34 } },
+                { 4, new List<int>() { 31, 32 } },
+                { 5, new List<int>() { 27, 28, 29, 30 } },
+                { 6, new List<int>() { 22, 23, 24, 25, 26, 27 } },
+                { 7, new List<int>() { 19, 20, 21, 22 } },
+                { 8, new List<int>() { 17, 18, 19 } },
+                { 9, new List<int>() { 11, 12, 13, 14, 15, 16, 17, 18 } },
+                { 10, new List<int>() { 6, 7, 8, 9, 10 } },
+                { 11, new List<int>() { 3, 4, 5 } },
+                { 12, new List<int>() { 55, 0, 1, 2 } },
+                { 13, new List<int>() { 48, 49, 50, 51, 52, 53, 54 } },
+                { 14, new List<int>() { 47, 48, 49 } },
+                { 15, new List<int>() { 43, 44, 45, 46, 47 } },
+            }
+        },
+
+        // // Level x
+        // { x, new Dictionary<int, List<int>>()
+        //     {
+        //         { 0, new List<int>() {} },
+        //         { 1, new List<int>() {} },
+        //         { 2, new List<int>() {} },
+        //         { 3, new List<int>() {} },
+        //         { 4, new List<int>() {} },
+        //         { 5, new List<int>() {} },
+        //         { 6, new List<int>() {} },
+        //         { 7, new List<int>() {} },
+        //         { 8, new List<int>() {} },
+        //         { 9, new List<int>() {} },
+        //         { 10, new List<int>() {} },
+        //         { 11, new List<int>() {} },
+        //         { 12, new List<int>() {} },
+        //         { 13, new List<int>() {} },
+        //         { 14, new List<int>() {} },
+        //         { 15, new List<int>() {} },
+        //     }
+        // },
     };
 }
